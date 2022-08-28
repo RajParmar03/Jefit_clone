6ee4b242b2286e94ae4032f109a3fb5ef5871b45
RajParmar03 <rjbusiness03@gmain.com>
2022-08-28 14:34:41 +0530

    solved blog page query.

--- scripts/login.js
+++ scripts/login.js
@@ -2,7 +2,10 @@ document.querySelector("form").addEventListener("submit",loginFunction);
 
 async function loginFunction(){
     event.preventDefault();
-    console.log("clicked");
-    // let form = document.querySelector("form");
-    // let
+    // console.log("clicked");
+    let form = document.querySelector("form");
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+
+    let result = fetch(" http://localhost:3000/posts")
 }
